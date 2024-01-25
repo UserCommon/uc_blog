82eef64cb30b6d2ed9d5c9fe38dfa379901a0cde
usercommon <kirilnenashev@gmail.com>
2024-01-26 00:45:09 +0300

    feat: basic project architecture

--- flake.nix
+++ flake.nix
@@ -22,13 +22,19 @@
           };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           nativeBuildInputs = with pkgs;
-            [ rustToolchain pkg-config ];
+            [ rustToolchain cargo-watch pkg-config ];
           buildInputs = with pkgs; [ openssl ];
         in
         with pkgs;
         {
           devShells.default = mkShell {
             inherit buildInputs nativeBuildInputs;
+
+            shellHook = ''
+              echo "Hey! <3"
+              echo "this is development shell for my blog!"
+            '';
+
           };
         }
       );
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use axum::ServiceExt;
 use axum::{routing::get, Router};
+mod handlers;
+use handlers::*;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().nest("/api", api_router());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
